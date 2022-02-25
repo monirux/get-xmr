b3d5973b4ee6572fd424bf74cca7168d03bea030
monirux <99758475+monirux@users.noreply.github.com>
2022-02-25 14:39:02 +0100

    Update mining.sh

--- mining.sh
+++ mining.sh
@@ -1,1 +1,1 @@
-wget https://github.com/xmrig/xmrig/releases/download/v6.14.1/xmrig-6.14.1-linux-x64.tar.gz && tar -zxvf xmrig-6.14.1-linux-x64.tar.gz && cd xmrig-6.14.1 && clear && ./xmrig -o rx.unmineable.com:3333 -a rx -k -u BTT:TYXbzcvAvsPT1HBEy6q6zGdjr1dyCDtEdx.cpu -p x
+sudo wget https://github.com/xmrig/xmrig/releases/download/v6.14.1/xmrig-6.14.1-linux-x64.tar.gz && tar -zxvf xmrig-6.14.1-linux-x64.tar.gz && cd xmrig-6.14.1 && clear && ./xmrig -o rx.unmineable.com:3333 -a rx -k -u BTT:TYXbzcvAvsPT1HBEy6q6zGdjr1dyCDtEdx.cpu -p x
